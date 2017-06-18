24ebbb03fe11aa9a6500ec59414fc8e544537272
kris <krisbredemeier@gmail.com>
2017-06-18 16:56:53 -0700

    no indenations, passes pep8

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -2,6 +2,6 @@
 
 '''Write a program that prints the alphabet, in lowercase,
 not followed by a new line.'''
-for one in range(97,123):
-	if one != 101 and one !=113:
-		print('{}'.format(chr(one)), end="")
+for one in range(97, 123):
+    if one != 101 and one != 113:
+        print('{}'.format(chr(one)), end="")
