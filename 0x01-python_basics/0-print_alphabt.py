d544b9b2c3979f827f711d013bf0f568d3bc8503
kris <krisbredemeier@gmail.com>
2017-06-18 16:48:49 -0700

    woks on pep8

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
+'''Write a program that prints the alphabet, in lowercase,
+not followed by a new line.'''
 for one in range(97,123):
 	if one != 101 and one !=113:
 		print('{}'.format(chr(one)), end="")
-		# print(chr(one), end ="")
