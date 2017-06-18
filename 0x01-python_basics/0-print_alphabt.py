65f28e8611033cc4ecec466acae6054f740b0a0a
kris <krisbredemeier@gmail.com>
2017-06-18 16:29:10 -0700

    checks syntax error

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for one in range(97,123):
-    if one != 101 and one !=113:
-        print('{}'.format(chr(one)))
+	if one != 101 and one !=113:
+		print('{}'.format(chr(one)), end="")
+		# print(chr(one), end ="")
