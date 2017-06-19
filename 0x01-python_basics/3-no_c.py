fdda45f14e917b32e9cb2febf56bdb1a4185f65b
kris <krisbredemeier@gmail.com>
2017-06-18 17:40:30 -0700

    prints anything but c and C:

--- 0x01-python_basics/3-no_c.py
+++ 0x01-python_basics/3-no_c.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+'''Write a function that removes all c and C of a string
+and return the new string.'''
+
+def no_c(str):
+    '''remove c and C'''
+    without = ''
+    for i in str:
+        if i == 'c' or i == 'C':
+            i = ""
+        without += i
+    print(without)
