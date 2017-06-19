d837ff97a5b960c574a8fe761635b1ae4fe615f7
kris <krisbredemeier@gmail.com>
2017-06-18 17:56:31 -0700

    I believe it imports the file correclty, but does not print

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_4.py import add(a, b)
+# from add_4.py import add(a, b)
+this = __import__('add_4')
 '''program imports function from file'''
 
 def addThem():
     a = 1
     b = 2
-    print('{} + {} = {}'(a, b, sum(a, b)))
+    print('{} + {} = {}'(a, b, this(a, b)))
