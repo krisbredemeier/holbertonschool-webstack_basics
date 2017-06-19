8c8892ea62c6df5eb00b8d81b012099e769f1d23
kris <krisbredemeier@gmail.com>
2017-06-18 22:06:46 -0700

    basic outline of prototype:

--- 0x01-python_basics/9-print_sorted_dictionary.py
+++ 0x01-python_basics/9-print_sorted_dictionary.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+'''Write a function that prints a dictionary by ordered keys.'''
+
+
+def print_sorted_dictionary(my_dict):
+
+
+if __name__ == '__main__':
+    print_sorted_dictionary()
