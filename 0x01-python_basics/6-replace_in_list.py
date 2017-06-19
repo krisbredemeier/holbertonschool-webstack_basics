11f26283f3d8de9d4877def2a7964cf73f676f48
kris <krisbredemeier@gmail.com>
2017-06-18 21:34:55 -0700

    fits pep8 standards

--- 0x01-python_basics/6-replace_in_list.py
+++ 0x01-python_basics/6-replace_in_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
-'''Write a function that replaces an element of a list at a specific position.'''
+'''Write a function that replaces an element
+of a list at a specific position.'''
+
 
 def replace_in_list(my_list, idx, element):
-    in idx < len(my_list) and idx >= 0:
+
+    ''' If idx is out of range, the function should not modify anything,
+     and returns the original list'''
+    if idx < len(my_list) and idx >= 0:
         my_list[idx] = element
     return(my_list)
 
