0efef2a9e45a687625a09d9dbc0f08219d7e6f66
kris <krisbredemeier@gmail.com>
2017-06-18 22:00:10 -0700

    outline of prototype

--- 0x01-python_basics/8-common_elements.py
+++ 0x01-python_basics/8-common_elements.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+'''Write a function that returns a set
+of common elements in two sets.'''
+
+
+def common_elements(set_1, set_2):
+
+
+if __name__ == '__main__':
+    common_elements()
