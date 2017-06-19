8713db875fa7459a93adc75b30e2cb7ff84b574c
kris <krisbredemeier@gmail.com>
2017-06-18 22:43:45 -0700

    make sure to use try, except, finally

--- 0x01-python_basics/11-safe_print_division.py
+++ 0x01-python_basics/11-safe_print_division.py
@@ -4,8 +4,12 @@
 
 
 def safe_print_division(a, b):
-
-
+    try:
+        (a / b)
+    except:
+        pass
+    finally:
+        print('Inside result: {}'.format)
 
 if __name__ == '__main__':
     safe_print_division()
