93d6251aa9c1f7401ad093a31156ab14b0b30e0f
kris <krisbredemeier@gmail.com>
2017-06-18 22:19:43 -0700

    removes, but not correctly yet

--- 0x01-python_basics/10-simple_delete.py
+++ 0x01-python_basics/10-simple_delete.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+'''Write a function that deletes a key in a dictionary.'''
+
+def simple_delete(my_dict, key=""):
+        remove = dict(my_dict)
+        del remove[key]
+        return remove
+
+
+if __name__ == '__main__':
+    simple_delete()
