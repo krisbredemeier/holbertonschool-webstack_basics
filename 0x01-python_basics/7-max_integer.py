b78777033e9274eb1c2d98025cd68618c254fe63
kris <krisbredemeier@gmail.com>
2017-06-18 21:46:57 -0700

    wokring though iteration

--- 0x01-python_basics/7-max_integer.py
+++ 0x01-python_basics/7-max_integer.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+''' Write a function that finds the biggest integer of a list.'''
+
+
+def max_integer(my_list=[]):
+    '''If the list is empty, return None'''
+    biggest = my_list[0]
+    if len(my_list) <= 0:
+        return None
+    for i in range(0, list(my_list)):
+        if biggest < my_list[i]:
+            biggest = my_list[i]
+    return(biggest)
+
+if __name__ == '__main__':
+    max_integer()
