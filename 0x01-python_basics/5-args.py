cf9641b44e8a6c9a23527aa3ac8523220616d1d0
kris <krisbredemeier@gmail.com>
2017-06-18 19:03:29 -0700

    prints args

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -4,9 +4,15 @@ import sys
 '''Write a program that prints the number of
 and the list of its arguments.'''
 
-arguments = "arguments"
-for arg in sys.argv:
-    if len(sys.argv) > 1:
+def numArgs():
+    arguments = "arguments"
+    if len(sys.argv) < 1:
         print('{} {}'.format(0, arguments))
     else:
-        print('0 Arguments')
+        print('{} {}'.format(len(sys.argv) -1, arguments))
+        for arg in range(len(sys.argv)):
+            if arg !=0:
+                print('{} {}'.format(arg, sys.argv[arg]))
+
+if __name__ == '__main__':
+    numArgs()
