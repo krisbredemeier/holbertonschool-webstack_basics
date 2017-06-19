e02ed3c8f15ede64c981c227d72e032eb381fcef
kris <krisbredemeier@gmail.com>
2017-06-18 18:00:58 -0700

    adds two variables from imported function

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # from add_4.py import add(a, b)
-this = __import__('add_4')
+add = __import__('add_4').add
 '''program imports function from file'''
 
 def addThem():
     a = 1
     b = 2
-    print('{} + {} = {}'(a, b, this(a, b)))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    addThem()
