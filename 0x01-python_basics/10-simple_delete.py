e3fc5db876acb18770ab8e0e8a99a0dd2efb1acd
kris <krisbredemeier@gmail.com>
2017-06-18 22:23:42 -0700

    uses try and except and it works

--- 0x01-python_basics/10-simple_delete.py
+++ 0x01-python_basics/10-simple_delete.py
@@ -2,10 +2,18 @@
 
 '''Write a function that deletes a key in a dictionary.'''
 
+
 def simple_delete(my_dict, key=""):
-        remove = dict(my_dict)
-        del remove[key]
-        return remove
+    '''key argument will be always a string'''
+    try:
+        del my_dict[key]
+    except:
+        pass
+    return(my_dict)
+
+        # remove = dict(my_dict)
+        # del remove[key]
+        # return remove
 
 
 if __name__ == '__main__':
