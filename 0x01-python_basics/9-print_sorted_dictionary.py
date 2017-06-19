ac9dcd12417debf2c3f3afe7c2ba2400a90b293d
kris <krisbredemeier@gmail.com>
2017-06-18 22:13:48 -0700

    removes excess space

--- 0x01-python_basics/9-print_sorted_dictionary.py
+++ 0x01-python_basics/9-print_sorted_dictionary.py
@@ -5,7 +5,7 @@
 
 def print_sorted_dictionary(my_dict):
     for key, value in sorted(my_dict.items()):
-        print('{} : {}'.format(key, value))
+        print('{}: {}'.format(key, value))
 
 
 if __name__ == '__main__':
