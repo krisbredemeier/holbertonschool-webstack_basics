01ac245b61e1e11119df3a46603198dbb47dc403
kris <krisbredemeier@gmail.com>
2017-06-18 17:10:44 -0700

    prints 0-99

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Write a program that prints numbers from 0 to 99'''
 
-for number in range(0, 99):
+for number in range(0, 100):
         # print('{}'.format(number), end=", ")
-        print(str(number).zfill(2))
+        if number == 99:
+            print('99')
+        print('{}'.format(str(number)).zfill(2), '', sep=", ", end="")
