99ef1733d20eb890be22cd318a60e67a71ec4dbc
kris <krisbredemeier@gmail.com>
2017-06-18 17:26:20 -0700

    basic funtion, but does not ditect upper

--- 0x01-python_basics/2-islower.py
+++ 0x01-python_basics/2-islower.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+'''Write a function that checks for lowercase character'''
+
+def islower(c):
+    '''check to see if letter is lowecase or not'''
+
+    if 97<=ord('c')<=122:
+         return(True)
+    return(False)
