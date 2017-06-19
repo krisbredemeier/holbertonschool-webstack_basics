d64a30910557b012dd9a0b63b06417f992cc271e
kris <krisbredemeier@gmail.com>
2017-06-18 21:28:10 -0700

    basic outline of replace element loop

--- 0x01-python_basics/6-replace_in_list.py
+++ 0x01-python_basics/6-replace_in_list.py
@@ -3,3 +3,6 @@
 '''Write a function that replaces an element of a list at a specific position.'''
 
 def replace_in_list(my_list, idx, element):
+    in idx < len(my_list):
+        my_list[inx] = element
+    return(my_list)
