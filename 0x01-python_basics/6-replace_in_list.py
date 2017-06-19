a9208cd086fc33d8bc5140e0302f0d032630ba26
kris <krisbredemeier@gmail.com>
2017-06-18 21:31:41 -0700

    adds conditon that idx must be greater than 0

--- 0x01-python_basics/6-replace_in_list.py
+++ 0x01-python_basics/6-replace_in_list.py
@@ -3,6 +3,9 @@
 '''Write a function that replaces an element of a list at a specific position.'''
 
 def replace_in_list(my_list, idx, element):
-    in idx < len(my_list):
-        my_list[inx] = element
+    in idx < len(my_list) and idx >= 0:
+        my_list[idx] = element
     return(my_list)
+
+if __name__ == '__main__':
+    replace_in_list()
