d67bede14156d13e74130dc70337a5b485154ecf
kris <krisbredemeier@gmail.com>
2017-06-18 22:03:49 -0700

    simple loop to find sets

--- 0x01-python_basics/8-common_elements.py
+++ 0x01-python_basics/8-common_elements.py
@@ -6,6 +6,11 @@ of common elements in two sets.'''
 
 def common_elements(set_1, set_2):
 
+    sets = []
+    for element in set_1:
+        if element in set_2:
+            sets.append(element)
+    return sets
 
 if __name__ == '__main__':
     common_elements()
