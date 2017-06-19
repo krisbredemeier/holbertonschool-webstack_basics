65a616ff3d6ad993ee5ce518e9f0f3e13512e5d0
kris <krisbredemeier@gmail.com>
2017-06-18 22:29:47 -0700

    removes excess comments, and protype for 11

--- 0x01-python_basics/10-simple_delete.py
+++ 0x01-python_basics/10-simple_delete.py
@@ -11,10 +11,5 @@ def simple_delete(my_dict, key=""):
         pass
     return(my_dict)
 
-        # remove = dict(my_dict)
-        # del remove[key]
-        # return remove
-
-
 if __name__ == '__main__':
     simple_delete()
--- 0x01-python_basics/11-safe_print_division.py
+++ 0x01-python_basics/11-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+'''Write a function that divides 2 integers and prints the result.'''
+
+
+def safe_print_division(a, b):
+
+
+
+if __name__ == '__main__':
+    safe_print_division()
