17d2adb9b1ca74ffc9ceb77c11eab02a9b1447f6
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-25 23:40:47 +0000

    implements use strict

--- 0x02-javascript_basics/1-arguments.js
+++ 0x02-javascript_basics/1-arguments.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+//Write a script that prints a message depending
+//of the number of arguments passed
+"use strict";
 function argvCounter() {
   for (var i = 0; i < 10; i++) {
     console.log(i)
@@ -6,3 +9,7 @@ function argvCounter() {
   console.log('arguments', i)
 }
 argvCounter()
+
+if no argument console.log('No argument')
+if 1 argument console.log('Argument found')
+if < 1 argument console.log('Arguments found')
