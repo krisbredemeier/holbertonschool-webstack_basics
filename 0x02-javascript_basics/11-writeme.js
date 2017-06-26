85c3dab996e31f398988295192600ee6b9e51eb1
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 03:40:13 +0000

    fixes styling

--- 0x02-javascript_basics/11-writeme.js
+++ 0x02-javascript_basics/11-writeme.js
@@ -2,10 +2,10 @@
 // Write a script that writes a string to a file
 'use strict';
 let text = process.argv[3];
-let filename = process.argv[2]
+let filename = process.argv[2];
 let fs = require('fs');
 
 fs.writeFile(filename, text, function (err) {
   if (err) throw err;
-  console.log('Saved!');
+  // console.log('Saved!');
 });
