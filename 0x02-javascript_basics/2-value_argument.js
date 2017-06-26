2a829eaa7f111e1a7a5af7bc3f0181e51b106da1
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 01:14:11 +0000

    fixes it all

--- 0x02-javascript_basics/2-value_argument.js
+++ 0x02-javascript_basics/2-value_argument.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-//Write a script that prints the first argument passed to it:
-"use strict";
+// Write a script that prints the first argument passed to it:
+'use strict';
 let arg = process.argv;
-//create counter
+// create counter
 let i = 0;
 
 while (arg[i]) {
-    i++;
+  i++;
 }
 if (i === 2) {
-   console.log('No argument');
- } else {
-  //prints first argument
+  console.log('No argument');
+} else {
+  // prints first argument
   console.log(arg[2]);
-  }
+}
