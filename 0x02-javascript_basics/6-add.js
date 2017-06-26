fc8e476321d3686afb0c67552f2dd2ee825e43f1
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 02:43:35 +0000

    adds two variables

--- 0x02-javascript_basics/6-add.js
+++ 0x02-javascript_basics/6-add.js
@@ -2,8 +2,6 @@
 // Write a function that returns the addition of 2 integers.
 'use strict';
 
-module.exports = {
-  add: functino() {
-    return (x+y);
+module.exports.add = function(x, y) {
+    return (x + y);
   }
-};
