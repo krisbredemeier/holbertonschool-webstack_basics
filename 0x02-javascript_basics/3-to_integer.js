79d84c513cefc2cd223800d2cb30758ad83d07b4
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 01:16:04 +0000

    fixes styling for this doc

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Write a script that prints "My number: "
-//if the first argument can be converted to an integer:
-"use strict";
+// if the first argument can be converted to an integer:
+'use strict';
 let arg = process.argv;
 
-if (isNaN(arg[2]) == true) {
-   console.log('Not a number');
- }
-if (isNaN(arg[2]) == false) {
-  //prints first argument
+if (isNaN(arg[2]) === true) {
+  console.log('Not a number');
+}
+if (isNaN(arg[2]) === false) {
+  // prints first argument
   console.log('My number:', arg[2]);
- }
+}
