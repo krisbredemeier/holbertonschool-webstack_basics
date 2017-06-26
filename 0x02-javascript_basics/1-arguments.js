374098da058b70c6e5c5418b3a81a369555f1884
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 02:49:49 +0000

    fixes styling

--- 0x02-javascript_basics/1-arguments.js
+++ 0x02-javascript_basics/1-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//Write a script that prints a message depending
-//of the number of arguments passed
-"use strict";
+// Write a script that prints a message depending
+// of the number of arguments passed
+'use strict';
 let argvLen = process.argv.length;
 
 if (argvLen < 3) {
-   console.log('No argument');
+  console.log('No argument');
 } else if (argvLen === 3) {
   console.log('Argument found');
 } else {
