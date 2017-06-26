b50804081664297bcd5b2f48339d408ea5294b05
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 03:35:41 +0000

    writes file

--- 0x02-javascript_basics/11-writeme.js
+++ 0x02-javascript_basics/11-writeme.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-print "Number of arguments: ", len(sys.argv)
+// Write a script that writes a string to a file
+'use strict';
+let text = process.argv[2];
+let fs = require('fs');
+
+fs.writeFile(text, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
