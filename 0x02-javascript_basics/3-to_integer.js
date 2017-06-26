23adb44c4ec4f85141ecd884343e54a52d68d4bb
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 00:41:03 +0000

    finally can tell the difference between numbers and notnumbers

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -3,16 +3,15 @@
 //if the first argument can be converted to an integer:
 "use strict";
 let arg = process.argv;
-//create counter
-let i = 0;
 
-function isNumber( arg ) {
-    return !isNaN( arg );
+function isNumber(arg) {
+    return !isNaN(arg);
 }
 
-if (isNumber = false) {
-   console.log('not a number');
- } else {
+if (isNaN(arg[2]) == true) {
+   console.log('Not a number');
+ }
+if (isNaN(arg[2]) == false) {
   //prints first argument
   console.log('My number:', arg[2]);
   }
