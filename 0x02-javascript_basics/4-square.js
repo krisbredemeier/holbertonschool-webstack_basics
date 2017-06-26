b4a6e575e0fbd311bbb4ec4abf700aa205abc93d
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 01:18:03 +0000

    fixes styling

--- 0x02-javascript_basics/4-square.js
+++ 0x02-javascript_basics/4-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-//Write a script that prints a square
-"use strict";
+// Write a script that prints a square
+'use strict';
 let arg = process.argv;
 let i, j, row;
 
-if (isNaN(arg[2]) == true) {
-   console.log('Missing size');
- } else {
-   //loop through argument number to print square
-   for (i = 0; i < arg[2]; i++) {
-     row = '';
-     for (j = 0; j < arg[2]; j++) {
-       row += 'X';
-     }
-       console.log(row);
-   }
-  //console.log('My number:', arg[2]);
- }
+if (isNaN(arg[2]) === true) {
+  console.log('Missing size');
+} else {
+   // loop through argument number to print square
+  for (i = 0; i < arg[2]; i++) {
+    row = '';
+    for (j = 0; j < arg[2]; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+  // console.log('My number:', arg[2]);
+}
