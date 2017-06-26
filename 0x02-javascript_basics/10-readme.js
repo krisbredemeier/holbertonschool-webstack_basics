ce21bf27a9b556912294de021bea7924d4c09256
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 03:46:47 +0000

    reads content of file

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+// Write a script that reads and prints the content of a file.
 'use strict';
 let text = process.argv[2];
 let fs = require('fs');
 
-fs.readFile(text, function (err, input) {
-  if (err) {
-    console.log('err - try again');
-  }
-  console.log(input);
+fs.readFile(text, function (err, data) {
+  //if (err) {
+  //  console.log('err - try again');
+  //}
+  console.log(data);
 });
