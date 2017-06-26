2b92731433140e24f477c62f70095778033993c3
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 02:47:05 +0000

    passes styling

--- 0x02-javascript_basics/6-add.js
+++ 0x02-javascript_basics/6-add.js
@@ -2,6 +2,6 @@
 // Write a function that returns the addition of 2 integers.
 'use strict';
 
-module.exports.add = function(x, y) {
-    return (x + y);
-  }
+module.exports.add = function (x, y) {
+  return (x + y);
+};
