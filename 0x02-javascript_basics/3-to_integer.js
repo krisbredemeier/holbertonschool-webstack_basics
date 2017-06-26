c94ba9a807201380755dee002c16414ab906fb27
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 00:21:20 +0000

    adds initial psudo code

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// Write a script that prints "My number: "
+//if the first argument can be converted to an integer:
+"use strict";
+let arg = process.argv;
+
+if (i === 2) {
+   console.log('No argument');
+ } else {
+  //prints first argument
+  console.log(arg[2]);
+  }
