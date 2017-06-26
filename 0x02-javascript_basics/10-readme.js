a2a548455c10fc8b0c30d1a1fd9b4c05509c26f8
Kris Bredemeier <krisbredemeier@gmail.com>
2017-06-26 03:25:59 +0000

    changes error handeling

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -4,6 +4,8 @@ let text = process.argv[2];
 let fs = require('fs');
 
 fs.readFile(text, function (err, input){
-  if (err) throw err;
+  if (err) {
+    console.log('err');
+  }
   console.log(input);
 });
