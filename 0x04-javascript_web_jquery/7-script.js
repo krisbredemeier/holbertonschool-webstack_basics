2ef2b7de995371102f50065d74373196caf134f2
kris <krisbredemeier@gmail.com>
2017-07-09 09:27:16 -0700

    uses jquery .git

--- 0x04-javascript_web_jquery/7-script.js
+++ 0x04-javascript_web_jquery/7-script.js
@@ -1,5 +1,3 @@
-$.get('http://swapi.co/api/people/5/?format=json', function() {
-  $('#character').each(function() {
-    $(this).href = (this).href.replace("5", "1");
-  });
+$.get('http://swapi.co/api/people/5/?format=json', function(data) {
+  $( "#character" ).html( data );
 });
