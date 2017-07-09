20ef8879b9774fbb64b96445ff4cf173cc2fc676
kris <krisbredemeier@gmail.com>
2017-07-09 08:43:53 -0700

    works on getting and replaceing parts of url

--- 0x04-javascript_web_jquery/7-script.js
+++ 0x04-javascript_web_jquery/7-script.js
@@ -1,5 +1,5 @@
-let url = 'http://swapi.co/api/people/5/?format=json'
-
-$('#character').each(function(){
-this.href = this.href.replace("5", "1");
+$.get('http://swapi.co/api/people/5/?format=json', function() {
+  $('#character').each(function() {
+    $(this).href = (this).href.replace("5", "1");
+  });
 });
