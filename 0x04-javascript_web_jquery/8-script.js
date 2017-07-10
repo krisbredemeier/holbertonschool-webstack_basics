79d79e92b98e444e9ce51652745dbae2b279c1de
kris <krisbredemeier@gmail.com>
2017-07-09 18:20:16 -0700

    trying to resolve pern errors

--- 0x04-javascript_web_jquery/8-script.js
+++ 0x04-javascript_web_jquery/8-script.js
@@ -1,8 +1,5 @@
-// $.get('http://swapi.co/api/films/?format=json', function(data) {
-//   let obj = jQuery.parseJSON( '{ "title": "A New Hope" }' );
-// });
 $.get('http://swapi.co/api/films/?format=json', function ( data ) {
   data.results.map(fucntion (x) {
-    $("#list_movies").append('<li>' + x.title + '</li>');
+    $('#list_movies').append('<li>' + x.title + '</li>');
   });
 });
