9505b4a11664f12e370ff12fb02dd100819b9b4e
kris <krisbredemeier@gmail.com>
2017-07-09 18:18:06 -0700

    works on getting and apending json

--- 0x04-javascript_web_jquery/8-script.js
+++ 0x04-javascript_web_jquery/8-script.js
@@ -1,3 +1,8 @@
-$.get('http://swapi.co/api/films/?format=json', function(data) {
-  let obj = jQuery.parseJSON( '{ "title": "A New Hope" }' );
+// $.get('http://swapi.co/api/films/?format=json', function(data) {
+//   let obj = jQuery.parseJSON( '{ "title": "A New Hope" }' );
+// });
+$.get('http://swapi.co/api/films/?format=json', function ( data ) {
+  data.results.map(fucntion (x) {
+    $("#list_movies").append('<li>' + x.title + '</li>');
+  });
 });
