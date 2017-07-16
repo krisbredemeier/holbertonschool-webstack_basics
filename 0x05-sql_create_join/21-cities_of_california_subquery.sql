e08e8dc7238e0937e863d307fb38b10b04a2ebbe
kris <krisbredemeier@gmail.com>
2017-07-16 09:28:15 -0700

    continues formatting

--- 0x05-sql_create_join/21-cities_of_california_subquery.sql
+++ 0x05-sql_create_join/21-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
-USE hbtn_0d_usa;
 SELECT id, name
-FROM cities
-WHERE state_id = (
-  SELECT id FROM states WHERE name = California);
+  FROM cities
+    WHERE state_id = (
+        SELECT id FROM states WHERE name = California);
