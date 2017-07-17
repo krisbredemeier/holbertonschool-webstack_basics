0221f5d72fa20d458ba2e47bb380300591fbda20
kris <krisbredemeier@gmail.com>
2017-07-17 10:09:45 -0700

    uses join, not left join

--- 0x05-sql_create_join/22-cities_by_state_join.sql
+++ 0x05-sql_create_join/22-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM `cities`
-LEFT JOIN `states` ON cities.state_id = states.id;
-ORDER BY citeis.id ASC;
+JOIN `states`
+ON cities.state_id = states.id;
+ORDER BY ASC citeis.id;
