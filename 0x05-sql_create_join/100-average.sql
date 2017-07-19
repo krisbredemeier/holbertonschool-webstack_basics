a69aa0796483828d9bed28e7a17eb55adb9288a3
kris <krisbredemeier@gmail.com>
2017-07-18 19:42:14 -0700

    inital code

---
+++ 0x05-sql_create_join/100-average.sql
@@ -0,0 +1,5 @@
+-- Write a script that computes the score average of all records
+-- in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- SELECT `second_table.scores`
+-- FROM `second_table`
+SELECT AVG(second_table.scores)
