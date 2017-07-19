67cf42dcb49a60e2b8c571a695b162bac7adb5d7
kris <krisbredemeier@gmail.com>
2017-07-18 18:56:37 -0700

    corrects typos

--- 0x05-sql_create_join/29-shows_by_genre.sql
+++ 0x05-sql_create_join/29-shows_by_genre.sql
@@ -5,5 +5,6 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_genre.id = tv_shows_genres.genre_id
-ORDER BY ASC tv_shows.title;
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title
+ASC;
