44ee70b2da43e7652a5022e2687c0d751fc11316
kris <krisbredemeier@gmail.com>
2017-07-18 18:53:14 -0700

    uses correct table names

--- 0x05-sql_create_join/28-comedy_only.sql
+++ 0x05-sql_create_join/28-comedy_only.sql
@@ -4,6 +4,7 @@ FROM tv_shows
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre.id
-WHERE tv_genre.names = "Comedy"
-ORDER BY ASC tv_shows.title;
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title
+ASC;
