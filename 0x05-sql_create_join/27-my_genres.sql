04685d202ee1f54f3d3884c95008a3a032ff9e17
kris <krisbredemeier@gmail.com>
2017-07-18 18:50:28 -0700

    corect table names used

--- 0x05-sql_create_join/27-my_genres.sql
+++ 0x05-sql_create_join/27-my_genres.sql
@@ -3,8 +3,19 @@
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
-ON tv_genre.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 Join tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = "Dexter"
-ORDER BY ASC tv_genre.name;
+ORDER BY tv_genres.name
+ASC;
+
+
+
+
+
+
+
+
+
+
