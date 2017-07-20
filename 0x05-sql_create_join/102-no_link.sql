8bb11182cc64b24dc20e5044fe719c991d037757
kris <krisbredemeier@gmail.com>
2017-07-20 16:42:47 -0700

    lista ll records

---
+++ 0x05-sql_create_join/102-no_link.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all records of the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+SELECT second_table.score, second_table.name
+FROM second_table
+ORDER BY second_table.score
+DESC;
