6e28799d17f5ebc73d1a14d269bea4a22d287030
kris <krisbredemeier@gmail.com>
2017-07-20 16:29:45 -0700

    counts how many reoccuring scores, and orders them

--- 0x05-sql_create_join/101-groups.sql
+++ 0x05-sql_create_join/101-groups.sql
@@ -1,11 +1,8 @@
 -- Write a script that lists the number of records
 -- with the same score in the table second_table of the database hbtn_0c_0
 -- in your MySQL server.
-SELECT second_table.score
+SELECT second_table.score,
+COUNT(*) AS `number`
 FROM second_table
-WHERE second_table.score IN (
-  SELECT second_table.score
-  FROM second_table
-  GROUP BY score
-  HAVING COUNT AS `number` > 1
-);
+GROUP BY score
+ORDER BY `number` DESC;
