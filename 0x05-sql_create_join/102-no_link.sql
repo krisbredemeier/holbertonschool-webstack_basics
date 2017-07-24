c2bab97c148d1dc54fbbad768ed3070719e3c5c1
kris <krisbredemeier@gmail.com>
2017-07-23 20:57:52 -0700

    usues where clause for a filter

--- 0x05-sql_create_join/102-no_link.sql
+++ 0x05-sql_create_join/102-no_link.sql
@@ -2,5 +2,6 @@
 -- of the database hbtn_0c_0 in your MySQL server.
 SELECT second_table.score, second_table.name
 FROM second_table
+WHERE second_table.name IS NOT NULL
 ORDER BY second_table.score
 DESC;
