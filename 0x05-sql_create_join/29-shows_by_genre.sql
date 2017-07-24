86bddab5e5fd744a40e724087a9bdefa1c64564f
kris <krisbredemeier@gmail.com>
2017-07-24 08:59:57 -0700

    sort by tv_genre.name

--- 0x05-sql_create_join/29-shows_by_genre.sql
+++ 0x05-sql_create_join/29-shows_by_genre.sql
@@ -6,5 +6,5 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title
+ORDER BY tv_shows.title, tv_genres.name
 ASC;
