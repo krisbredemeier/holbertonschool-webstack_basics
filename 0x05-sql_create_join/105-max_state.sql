1d91193dd1037a2dbd4dd6ff203400caa30216f7
kris <krisbredemeier@gmail.com>
2017-07-23 21:40:16 -0700

    displays max temps

---
+++ 0x05-sql_create_join/105-max_state.sql
@@ -0,0 +1,6 @@
+-- Write a script that displays the max temperature of each state.
+SELECT temperatures.state,
+MAX(temperatures.value) AS `max_temp`
+FROM temperatures
+GROUP BY temperatures.state
+ORDER BY temperatures.state;
