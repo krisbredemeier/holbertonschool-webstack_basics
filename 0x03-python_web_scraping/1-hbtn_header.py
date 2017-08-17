ea59cf2c69c0a9e97b8578f819d54d3f31430cff
kris <krisbredemeier@gmail.com>
2017-08-17 15:28:03 -0700

    complies with pep8

--- 0x03-python_web_scraping/1-hbtn_header.py
+++ 0x03-python_web_scraping/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-import requests, sys
+import requests
+import sys
 
 '''
 Write a Python script that takes in a URL, sends a request
@@ -8,6 +9,5 @@ to the URL and displays the value of the variable X-Request-Id
 in the response header
 '''
 
-url = (sys.argv[1])
 h = requests.get(sys.argv[1])
 print(h.headers['x-request-id'])
