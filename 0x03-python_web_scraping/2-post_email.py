f75c4e82de06f51fb289b4ddd03b60759207e7bc
kris <krisbredemeier@gmail.com>
2017-08-17 15:48:16 -0700

    container

--- 0x03-python_web_scraping/2-post_email.py
+++ 0x03-python_web_scraping/2-post_email.py
@@ -9,8 +9,8 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 '''
 
-url = (sys.argv[1])
-email = (sys.argv[2])
+url = requests.get(sys.argv[1])
+email = requests.get(sys.argv[2])
 # h = requests.get(sys.argv[1])
 
 # url.requests("POST", email)
