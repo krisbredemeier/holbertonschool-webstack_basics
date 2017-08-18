9c44573bf59dfe5e917bc006abc0efdeb40d6626
kris <krisbredemeier@gmail.com>
2017-08-18 15:28:22 -0700

    container

--- 0x03-python_web_scraping/2-post_email.py
+++ 0x03-python_web_scraping/2-post_email.py
@@ -9,12 +9,31 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 '''
 
+
+# GET
 url = requests.get(sys.argv[1])
 email = requests.get(sys.argv[2])
-# h = requests.get(sys.argv[1])
+# url = (sys.argv[1])
+# email = (sys.argv[2])
+
+print(url)
+print(email)
+
+# POST
+# rp = requests.post(url, email)
+
+# response, status
+# print(url.text)
+# print(email.text)
+# print(rp.text)
+# print(url.status_code)
+# print(rp.status_code)
+
+
+
 
 # url.requests("POST", email)
-h = requests.post(email)
-print('Your email is:', email)
+# h = requests.post(email)
+# print('Your email is:', email)
 # print(url)
 # print(email)
