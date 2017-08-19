950de4f8cac6d6002fba437f19d5ecc2b4fcd7df
kris <krisbredemeier@gmail.com>
2017-08-18 17:26:39 -0700

    starting to uderstand servers and port maping

--- 0x03-python_web_scraping/2-post_email.py
+++ 0x03-python_web_scraping/2-post_email.py
@@ -11,29 +11,14 @@ and finally displays the body of the response.
 
 
 # GET
-url = requests.get(sys.argv[1])
-email = requests.get(sys.argv[2])
-# url = (sys.argv[1])
-# email = (sys.argv[2])
+url = (sys.argv[1])
+urlr = requests.get(url)
+email = (sys.argv[2])
 
-print(url)
-print(email)
-
-# POST
-# rp = requests.post(url, email)
-
-# response, status
-# print(url.text)
-# print(email.text)
-# print(rp.text)
-# print(url.status_code)
-# print(rp.status_code)
-
-
-
-
-# url.requests("POST", email)
-# h = requests.post(email)
-# print('Your email is:', email)
 # print(url)
 # print(email)
+
+# POST
+rp = requests.post(url, email)
+
+print('Your email is:', email)
