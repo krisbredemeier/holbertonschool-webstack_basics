983a2c8da250da994942acea80d0a47e0ea9c66e
kris <krisbredemeier@gmail.com>
2017-08-18 22:36:31 -0700

    wont work

--- 0x03-python_web_scraping/5-starwars.py
+++ 0x03-python_web_scraping/5-starwars.py
@@ -5,14 +5,21 @@ Write a Python script that takes in a string
 and sends a search request to the Star Wars API
 '''
 
-import requests
-import sys
+def starwars():
+    '''
+    all of the planets
+    '''
+    import requests
+    import sys
 
-person = sys.argv[1]
-url = requests.puts('https://swapi.co/api/people/name/?search')
-name = 
-count = (url )
+    person = sys.argv[1]
+    url = ('https://swapi.co/api/people/name/?search')
+    urlr = requests.get(url, params={ 'search': person }).json()
+    print(urlr)
 
+    print('Number of results: {}'.format(urlr['count']))
+    for name in urlr['results']:
+        print(name['name'])
 
-print('Number of results: ', count)
-print(name)
+if __name__ == "__main__":
+    starwars()
