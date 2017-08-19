d7554702cb7fa9afd99baa1d070595ce781499ff
kris <krisbredemeier@gmail.com>
2017-08-18 17:37:25 -0700

    checks body response

--- 0x03-python_web_scraping/3-error_code.py
+++ 0x03-python_web_scraping/3-error_code.py
@@ -4,9 +4,16 @@ import requests
 import sys
 
 '''
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
 
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
 '''
 url = (sys.argv[1])
 urlr = requests.get(url)
 
-print(urlr.text)
+if (urlr.status_code >= 400):
+    print('Error code:', urlr.text)
+else:
+    print(urlr.text)
