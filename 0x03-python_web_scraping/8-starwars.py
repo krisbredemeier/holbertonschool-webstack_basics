41cbccd87c0ebb6955b72c9eadab42ea6eaf2eac
kris <krisbredemeier@gmail.com>
2017-09-07 13:00:05 -0700

    starwars with pagnation

--- 0x03-python_web_scraping/8-starwars.py
+++ 0x03-python_web_scraping/8-starwars.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+import sys
+import requests
+
+def sw_api():
+    '''
+    Write a Python script that takes in a string
+    and sends a search request to the Star Wars API
+    '''
+
+    person = sys.argv[1]
+    url = ('https://swapi.co/api/people')
+    urlr = requests.get(url, params={ 'search': person }).json()
+
+    print('Number of results: {}'.format(urlr['count']))
+    # count the number of resturns
+    for name in urlr['results']:
+        print(name['name'])
+
+if __name__ == "__main__":
+    sw_api()
