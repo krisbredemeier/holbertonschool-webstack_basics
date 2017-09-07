4e84095a97eb2395f38b930de03649ec3a5c2c26
kris <krisbredemeier@gmail.com>
2017-09-07 09:38:54 -0700

    basic outline

--- 0x03-python_web_scraping/6-my_github.py
+++ 0x03-python_web_scraping/6-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+'''
+Write a Python script
+that takes your Github credentials (username and password)
+and uses the Github API to display your id
+'''
+
+def github():
+    '''
+    Github
+    '''
+    curl -u "krisbredemeier" https://api.github.com
+
+if __name__ == "__main__":
+    github()
