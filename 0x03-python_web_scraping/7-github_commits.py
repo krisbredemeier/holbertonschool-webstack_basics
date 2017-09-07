4867d56106f7c617be9f23e9e101cf68afb51d4a
kris <krisbredemeier@gmail.com>
2017-09-07 12:54:09 -0700

    top ten

--- 0x03-python_web_scraping/7-github_commits.py
+++ 0x03-python_web_scraping/7-github_commits.py
@@ -7,11 +7,12 @@ def last_ten():
     url = 'https://api.github.com/repos'
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url_repo = (url + owner_name + repo_name + 'commits')
-    urlr = requests.get(url_repo, auth=(repo_name, owner_name)).json()
-    top = urlr[:10]
-    for commit in top:
-        print('{}:{}'.format(commit['sha'], commit['commit']['author']['name']))
+    url_repo = (url + owner_name + '/' + repo_name + '/' + 'commits')
+    print(url_repo)
+    urlr = requests.get(url_repo).json()
+    # top = urlr[10]
+    for commit in range(10):
+        print('{}: {}'.format(commit['sha'], commit['commit']['author']['name']))
 
 if __name__ == "__main__":
     last_ten()
