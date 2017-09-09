7fdcc27be6edf43c6ae672813930f2c58433aaad
kris <krisbredemeier@gmail.com>
2017-09-09 10:23:39 -0700

    uses tabs

--- 0x03-python_web_scraping/0-hbtn_status.py
+++ 0x03-python_web_scraping/0-hbtn_status.py
@@ -10,12 +10,12 @@ def status():
     '''
     beginning with get response
     '''
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get('https://intranet.hbtn.io/status')
     t = (type(r.text))
     c = r.text
     print('Body response:')
-    print('    - type:', t)
-    print('    - content:', c)
+    print('\t- type:', t)
+    print('\t- content:', c)
 
 if __name__ == "__main__":
     status()
